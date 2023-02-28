7d2a09b84b51827e82cc01c5bc8e2df7673b7efb
Pavel Kukushkin <pavelkukushkin@hotmail.com>
2023-02-28 20:51:27 +0100

    Domaca Uloha 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import {Component} from "@angular/core";
 
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
+@Component({ selector: 'app-root', templateUrl:
+    './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'library-fe';
+  binaryArray: number[] = [0, 0, 0, 0, 0, 0, 0, 0];
+  decimalNumber: number = 0;
+
+  updateCheckboxes(value: string) {
+    this.decimalNumber = parseInt(value);
+    for (let i = 0; i < 8; i++) {
+      this.binaryArray[7-i] = (this.decimalNumber & (1 << i)) >> i;
+    }
+  }
+
+  updateCheckbox(index: number) {
+    this.binaryArray[index] = this.binaryArray[index] === 1 ? 0 : 1;
+    this.decimalNumber = 0;
+    for (let i = 0; i < 8; i++) {
+      this.decimalNumber += this.binaryArray[7-i] << i;
+    }
+  }
 }
